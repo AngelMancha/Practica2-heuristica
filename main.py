fa35420e94c8772b8901084dad38c5cc8e8aa0b5
SU-NOMBRE-DE-USUARIO-DE-GITUHUB <SU-CORREO-ELECTRONICO>
2022-11-26 21:09:14 +0100

    comprobacion asientos adyacentes parte2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ problem.addVariable('alumno_3', [1, 2, 3, 4])
 
 # Primera restricción: Todos los alumnos tienen que tener asignado un asiento
 
-def todosAlumnosConAsiento(*args):
+def todosAlumnosConAsiento(*args: list) -> bool:
     #return all(alumno in list(args) for alumno in [1, 2, 3])
     for alumno in list(args):
         if alumno not in domain :
@@ -26,7 +26,7 @@ problem.addConstraint(todosAlumnosConAsiento, (['alumno_1', 'alumno_2', 'alumno_
 
 # Segunda restricción: Los alumnos de mov reducida deben dejar el asiendo de al lado libre
 
-def no_al_lado(alumno_red, alumno_normie):
+def no_al_lado(alumno_red: int, alumno_normie: int) -> bool:
     if alumno_red % 2 == 0 and alumno_normie != alumno_red - 1:
         return True
     if alumno_red % 2 != 0 and alumno_normie != alumno_red + 1:
@@ -37,16 +37,20 @@ problem.addConstraint(no_al_lado, ('alumno_1', 'alumno_2'))
 problem.addConstraint(no_al_lado, ('alumno_1', 'alumno_3'))
 
 # Tercera restricción: impide que un mismo asiento se le asigne a 2 alumnos
-def not_encima(alumno_1, alumno_2):
+def not_encima(alumno_1: int, alumno_2: int) -> bool:
     if alumno_1 != alumno_2:
         return True
+
+problem.addConstraint(not_encima, ('alumno_1', 'alumno_2'))
+problem.addConstraint(not_encima, ('alumno_1', 'alumno_3'))
+problem.addConstraint(not_encima, ('alumno_2', 'alumno_3'))
 
 # Cuarta restricción: Si un asiento para una persona de movilidad reducida no está asignado, cualquier otro alumno
 # se puede sentar en ese asiento: viene implícito en la forma en la que se ha modelado el problema
 
 
 # Quinta restricción: No puede haber 2 alumnos conflictivos juntos en los asientos adyacentes
-def comprobar_asientos_adyacentes(alumno_conflictivo1, alumno_conflictivo2):
+def comprobar_asientos_adyacentes(alumno_conflictivo1: int, alumno_conflictivo2: int) -> bool:
     for asiento in domain:
         if alumno_conflictivo1 == asiento:
             # comprobar asientos contiguos
@@ -60,8 +64,4 @@ def comprobar_asientos_adyacentes(alumno_conflictivo1, alumno_conflictivo2):
                 return False
 
 
-
-problem.addConstraint(not_encima, ('alumno_1', 'alumno_2'))
-problem.addConstraint(not_encima, ('alumno_1', 'alumno_3'))
-problem.addConstraint(not_encima, ('alumno_2', 'alumno_3'))
 print(problem.getSolutions())
