a25acead08f31eed1d13a653c398df1d5d95b1a3
AngelMancha <100451302@alumnos.uc3m.es>
2022-11-27 21:07:22 +0100

    Solo falta restricción hermanos

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ problem = Problem()
 #4 conflictivo
 #5 con
 
-domain =[1,2,3, 4, 5]
-asientos = [1, 2, 3, 4, 5, 6, 7, 8]
+dominio_reducido = [1, 2]
+domain_ciclo1 = [1, 2, 3, 4, 5 ,6 ,7 ,8]
+domain_ciclo2 = [9, 10, 11, 12]
+asientos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-problem.addVariable('alumno_1', [1, 2])
-problem.addVariable('alumno_2', asientos)
-problem.addVariable('alumno_3', asientos)
-problem.addVariable('alumno_conflictivo4', asientos)
-problem.addVariable('alumno_conflictivo5', asientos)
+problem.addVariable('alumno_1', dominio_reducido)
+problem.addVariable('alumno_2', domain_ciclo1)
+problem.addVariable('alumno_3', domain_ciclo1)
+problem.addVariable('alumno_conflictivo4', domain_ciclo2)
+problem.addVariable('alumno_conflictivo5', domain_ciclo2)
+problem.addVariable('alumno_ciclo1', domain_ciclo1 )
+problem.addVariable('alumno_ciclo2', domain_ciclo1)
 # Primera restricción: Todos los alumnos tienen que tener asignado un asiento
 
 def todosAlumnosConAsiento(*args: list) -> bool:
@@ -53,25 +57,39 @@ problem.addConstraint(not_encima, ('alumno_1', 'alumno_2'))
 problem.addConstraint(not_encima, ('alumno_1', 'alumno_3'))
 problem.addConstraint(not_encima, ('alumno_1', 'alumno_conflictivo4'))
 problem.addConstraint(not_encima, ('alumno_1', 'alumno_conflictivo5'))
+problem.addConstraint(not_encima, ('alumno_1', 'alumno_ciclo1'))
+problem.addConstraint(not_encima, ('alumno_1', 'alumno_ciclo2'))
 problem.addConstraint(not_encima, ('alumno_2', 'alumno_3'))
 problem.addConstraint(not_encima, ('alumno_2', 'alumno_conflictivo4'))
 problem.addConstraint(not_encima, ('alumno_2', 'alumno_conflictivo5'))
+problem.addConstraint(not_encima, ('alumno_2', 'alumno_ciclo1'))
+problem.addConstraint(not_encima, ('alumno_2', 'alumno_ciclo2'))
 problem.addConstraint(not_encima, ('alumno_3', 'alumno_conflictivo4'))
 problem.addConstraint(not_encima, ('alumno_3', 'alumno_conflictivo5'))
+problem.addConstraint(not_encima, ('alumno_3', 'alumno_ciclo1'))
+problem.addConstraint(not_encima, ('alumno_3', 'alumno_ciclo2'))
 problem.addConstraint(not_encima, ('alumno_conflictivo4', 'alumno_conflictivo5'))
+problem.addConstraint(not_encima, ('alumno_conflictivo4', 'alumno_ciclo1'))
+problem.addConstraint(not_encima, ('alumno_conflictivo4', 'alumno_ciclo2'))
+problem.addConstraint(not_encima, ('alumno_conflictivo5', 'alumno_ciclo1'))
+problem.addConstraint(not_encima, ('alumno_conflictivo5', 'alumno_ciclo2'))
+problem.addConstraint(not_encima, ('alumno_ciclo1', 'alumno_ciclo2'))
+problem.addConstraint(not_encima, ('alumno_ciclo1', 'alumno_ciclo2'))
+problem.addConstraint(not_encima, ('alumno_ciclo1', 'alumno_ciclo2'))
 
 # Cuarta restricción: Si un asiento para una persona de movilidad reducida no está asignado, cualquier otro alumno
 # se puede sentar en ese asiento: viene implícito en la forma en la que se ha modelado el problema
 
 primera_fila = [1, 2, 3, 4]
-ultima_fila = [5, 6, 7, 8]
+ultima_fila = [9, 10, 11, 12]
 
 # Quinta restricción: No puede haber 2 alumnos conflictivos juntos en los asientos adyacentes
 def comprobar_asientos_adyacentes(alumno_conflictivo1: int, alumno_conflictivo2: int) -> bool:
     for asiento in asientos:
         if alumno_conflictivo1 == asiento:
+            #ASIENTOS PARES
             if alumno_conflictivo1 % 2 == 0:
-                #asientos que no están en la primera fila
+                #asientos está en la primera fila
                 if asiento in primera_fila:
                     # comprobamos el caso que el asiento sea 2
                     if asiento == 2 and (alumno_conflictivo2 == 5 or alumno_conflictivo2 == 6 or alumno_conflictivo2 == 7 or \
@@ -81,10 +99,12 @@ def comprobar_asientos_adyacentes(alumno_conflictivo1: int, alumno_conflictivo2:
                     if asiento == 4 and (alumno_conflictivo2 == 7 or alumno_conflictivo2 == 3 or alumno_conflictivo2 == 8) :
                         return False
 
+                #asiento está en la última fila
                 if asiento in ultima_fila:
-
-                    if (asiento-1)%4 == 0 and (alumno_conflictivo2 == asiento + 1 or alumno_conflictivo2 == asiento -4 or alumno_conflictivo2 == asiento -3):
+                    #alumno en ventana inferior
+                    if (asiento)%4 == 0 and (alumno_conflictivo2 == asiento - 1 or alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento -4):
                         return False
+                    #no está en ventana inferior
                     else:
                         if alumno_conflictivo2 == asiento -1 or alumno_conflictivo2 == asiento +1 or \
                                 alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento -4 or \
@@ -131,31 +151,42 @@ def comprobar_asientos_adyacentes(alumno_conflictivo1: int, alumno_conflictivo2:
                         return False
 
                 if asiento in ultima_fila:
+                    #asiento está en la ventana superior
                     if (asiento-1)%4 == 0 and (alumno_conflictivo2 == asiento + 1 or
                                                alumno_conflictivo2 == asiento -4 or alumno_conflictivo2 == asiento -3):
                         return False
-
-                    if alumno_conflictivo2 == asiento -1 or alumno_conflictivo2 == asiento +1 or \
-                            alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento -4 or \
-                            alumno_conflictivo2 == asiento -3:
-                        return False
-                # en otro caso no estamos en la primera fila y en ventana
-                if (asiento-1)%4 == 0:
-                    if alumno_conflictivo2 == asiento - 3 or alumno_conflictivo2 == asiento - 4 or \
-                            alumno_conflictivo2 == asiento + 1 or alumno_conflictivo2 == asiento + 4 or \
-                            alumno_conflictivo2 == asiento + 5:
-                        return False
-
-            # en otro caso
-            if alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento - 4 \
-                    or alumno_conflictivo2 == asiento - 3 \
-                    or alumno_conflictivo2 == asiento - 1 or alumno_conflictivo2 == asiento + 1 \
-                    or alumno_conflictivo2 == asiento + 3 or alumno_conflictivo2 == asiento + 4 \
-                    or alumno_conflictivo2 == asiento +5:
-                return False
+                    #asiento no está en la ventana superior
+                    else:
+                        if alumno_conflictivo2 == asiento -1 or alumno_conflictivo2 == asiento +1 or \
+                                alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento -4 or \
+                                alumno_conflictivo2 == asiento -3:
+                            return False
+                # no estamos en la primera fila ni en la última fila
+                else:
+                    #si estamos en la ventana superior
+                    if (asiento-1)%4 == 0:
+                        if alumno_conflictivo2 == asiento - 3 or alumno_conflictivo2 == asiento - 4 or \
+                                alumno_conflictivo2 == asiento + 1 or alumno_conflictivo2 == asiento + 4 or \
+                                alumno_conflictivo2 == asiento + 5:
+                            return False
+                    else:
+                        if alumno_conflictivo2 == asiento -5 or alumno_conflictivo2 == asiento - 4 \
+                                or alumno_conflictivo2 == asiento - 3 \
+                                or alumno_conflictivo2 == asiento - 1 or alumno_conflictivo2 == asiento + 1 \
+                                or alumno_conflictivo2 == asiento + 3 or alumno_conflictivo2 == asiento + 4 \
+                                or alumno_conflictivo2 == asiento +5:
+                            return False
 
     return True
 
 problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_conflictivo4', 'alumno_conflictivo5'))
+problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_conflictivo4', 'alumno_1'))
+problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_conflictivo5', 'alumno_1'))
+problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_2', 'alumno_1'))
+problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_2', 'alumno_conflictivo4'))
+problem.addConstraint(comprobar_asientos_adyacentes, ('alumno_2', 'alumno_conflictivo5'))
+
+# Sexta restricción: alumnos de 1er ciclo alante, los tontitos viejos atras
+
 
 print(problem.getSolutions())
