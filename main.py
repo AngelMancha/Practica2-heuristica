c1380978e2c491e58a91ade55552dd9dd379d234
SU-NOMBRE-DE-USUARIO-DE-GITUHUB <SU-CORREO-ELECTRONICO>
2022-12-07 16:50:48 +0100

    abrir fichero

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 PATH: str = 'CSP-tests/'
-
+alumnos = []
 
 with open('CSP-tests/alumnos1.txt') as file:
-    alumnos = []
+
     alum, al = [], []
     for linea in file:
         al.clear()
-        alum.clear()
-        print(alum)
-        print(alumnos)
         al = linea.split(sep=', ')
         for element in al:
             if element.isdigit():
                 alum.append(int(element))
             if not element.isdigit():
                 if '\n' in element:
+
                     element = int(element[0])
                 alum.append(element)
         alumnos.append(alum)
-        print(alum)
-        print(alumnos)
 
-
-
-#print(alumnos)
+print(alumnos)
