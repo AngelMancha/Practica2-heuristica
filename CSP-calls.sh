9befa88528ab4ac7ab26eb35f0aeb73b7d3f96c8
AngelMancha <100451302@alumnos.uc3m.es>
2022-12-09 02:42:56 +0100

    Casos de prueba y shell script

---
+++ CSP-calls.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+py CSPCargaBUS.py CSP-tests/alumnos1.txt
+py CSPCargaBUS.py CSP-tests/alumnos2.txt
+py CSPCargaBUS.py CSP-tests/alumnos3.txt
+py CSPCargaBUS.py CSP-tests/alumnos4.txt
+py CSPCargaBUS.py CSP-tests/alumnos5.txt
+py CSPCargaBUS.py CSP-tests/alumnos6.txt
+
