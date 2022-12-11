8fe85134ca0f88ce2ebbc028f8016707069781ce
AngelMancha <100451302@alumnos.uc3m.es>
2022-12-11 04:21:37 +0100

    queue A*

---
+++ queue.py
@@ -0,0 +1,31 @@
+# Implementacion de una cola en Python
+
+
+class Queue:
+    """Implementacion de una cola"""
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def display(self):
+        return self.items
+
+
+queue = Queue()
+queue.enqueue(210)
+queue.enqueue(0)
+queue.enqueue(20)
+queue.enqueue(1212)
+result = queue.dequeue()
+print(result)
