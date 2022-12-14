6a9d43e9362ed121e52e52db6700b41b348633cf
RuthNavarroCarrasco <SU-CORREO-ELECTRONICO>
2022-12-14 22:14:56 +0100

    DISLEXIA ARREGLADA.

--- CSPCargaBus.py
+++ CSPCargaBus.py
@@ -74,6 +74,7 @@ def assign_domain() -> None:
                 asignar_dom_c2(id_alumno)
             if hermano != 0:
                 asignar_dom_hermanos(id_alumno, hermano-1, ciclo)
+        print(dict_alumnos)
         problem.addVariable(id_alumno, dict_alumnos[id_alumno])
 
 
@@ -97,7 +98,8 @@ def asignar_dom_hermanos(id_alumno: str, hermano: int, ciclo: int) -> None:
     """Esta función le asigna el dominio correspondiente a un par de hermanos"""
     id_hermano, ciclo_hermano, conflictivo_hermano, movilidad_hermano = alumnos[hermano][0], alumnos[hermano][1], \
         alumnos[hermano][2], alumnos[hermano][3]
-    id_h = str(id_hermano) + str(movilidad_hermano) + str(conflictivo_hermano)
+    id_h = str(id_hermano) + str(conflictivo_hermano) + str(movilidad_hermano)
+    print(id_h)
     if ('R' in id_h and ciclo_hermano == 2) or ('R' in id_alumno and ciclo == 2):
         asignar_dom_c2(id_alumno)
         asignar_dom_c2(id_h)
@@ -129,7 +131,7 @@ def get_ciclo(datos_alumno) -> int:
 
 def get_movilidad(datos_alumno) -> int:
     """Esta función obtiene la movilidad de un alumno"""
-    if datos_alumno[1] == 'R':
+    if datos_alumno[2] == 'R':
         return True
     return False
 
